1d834d3999475440e8659bf14bc68f5adacb44d9
Nicholas Berlette <nick@berlette.com>
2022-08-02 23:36:45 -0700

    ⚡️remove collections/mod.ts export

--- deps.ts
+++ deps.ts
@@ -1,12 +1,10 @@
 export * as colors from "https://deno.land/std@0.150.0/fmt/colors.ts";
-export {
-  chunk,
-  deepMerge,
-  mapEntries,
-  mapKeys,
-  mapNotNullish,
-  mapValues,
-} from "https://deno.land/std@0.150.0/collections/mod.ts";
+export { chunk } from "https://deno.land/std@0.150.0/collections/chunk.ts";
+export { deepMerge } from "https://deno.land/std@0.150.0/collections/deep_merge.ts";
+export { mapEntries } from "https://deno.land/std@0.150.0/collections/map_entries.ts";
+export { mapValues } from "https://deno.land/std@0.150.0/collections/map_values.ts";
+export { mapKeys } from "https://deno.land/std@0.150.0/collections/map_keys.ts";
+export { mapNotNullish } from "https://deno.land/std@0.150.0/collections/map_not_nullish.ts";
 export * from "https://deno.land/x/case@2.1.1/mod.ts";
 export * as date from "https://deno.land/std@0.150.0/datetime/mod.ts";
 export * as dotenv from "https://deno.land/std@0.150.0/dotenv/mod.ts";
