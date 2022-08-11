074e71e3aba102601150599e440086a3361b3923
Nicholas Berlette <nick@berlette.com>
2022-08-11 04:20:50 -0700

    Add throttle test

--- src/throttle.ts
+++ src/throttle.ts
@@ -4,7 +4,6 @@ import { type Fn } from "./type.ts";
 
 /**
  * Promised `setTimeout`
- *
  * @category Promise
  */
 export function sleep(ms: number, callback?: Fn<void>) {
@@ -20,18 +19,22 @@ export function sleep(ms: number, callback?: Fn<void>) {
  * Throttle execution of a function. Especially useful for rate limiting
  * execution of handlers on events like resize and scroll.
  *
- * @param  {Function} callback - A function to be executed after delay milliseconds. The `this`
- * context and all arguments are passed through, as-is, to `callback` when the function is executed.
- * @param  {number} [delay=250] - A zero-or-greater delay in milliseconds.
- * For event callbacks, values around 100 or 250 (or even higher) are most useful.
- * @param  {boolean} [debounceMode=false] - If `debounceMode` is true (at begin), schedule `clear` to
- * execute after `delay` ms. If `debounceMode` is false, schedule `callback` to execute after `delay` ms.
- * @param  {boolean} [noTrailing=false] - Optional, defaults to false. If noTrailing is true,
+ * @param callback - A function to be executed after delay milliseconds. The
+ * `this` context and all arguments are passed through, as-is, to `callback`
+ * when the function is executed.
+ * @param delay - A zero-or-greater delay in milliseconds.
+ * For event callbacks, values around 100 or 250 (or even higher) most useful.
+ * @param debounceMode - If `debounceMode` is true (at begin), schedule `clear`
+ * to execute after `delay` ms. If `debounceMode` is false, schedule `callback`
+ * to execute after `delay` ms.
+ * @param noTrailing - Optional, defaults to false. If noTrailing is true,
  * callback will only execute every `delay` milliseconds while the
- * throttled-function is being called. If noTrailing is false or unspecified, callback
- * will be executed one final time after the last throttled-function call. (After the
- * throttled-function has not been called for `delay` milliseconds, the internal counter is reset).
- * @returns {Function} A new, throttled, function.
+ * throttled-function is being called. If noTrailing is false or unspecified,
+ * callback will be executed one final time after the last throttled-function
+ * call. (After the throttled-function has not been called for `delay`
+ * milliseconds, the internal counter is reset).
+ * @returns A new, throttled, function.
+ * @see {@link debounce}
  */
 export function throttle(
   callback: Fn | ((...args: any[]) => any),
@@ -121,19 +124,24 @@ export function throttle(
  * guarantees that a function is only executed a single time, either at the
  * very beginning of a series of calls, or at the very end.
  *
- * @param  {number}   delay -         A zero-or-greater delay in milliseconds. For event callbacks, values around 100 or 250 (or even higher) are most useful.
- * @param  {boolean}  [atBegin] -     Optional, defaults to false. If atBegin is false or unspecified, callback will only be executed `delay` milliseconds
- *                                  after the last debounced-function call. If atBegin is true, callback will be executed only at the first debounced-function call.
- *                                  (After the throttled-function has not been called for `delay` milliseconds, the internal counter is reset).
- * @param  {Function} callback -      A function to be executed after delay milliseconds. The `this` context and all arguments are passed through, as-is,
- *                                  to `callback` when the debounced-function is executed.
- *
- * @returns {Function} A new, debounced function.
+ * @param delay - Zero-or-greater delay in milliseconds. For event callbacks,
+ * values around 100 or 250 (or even higher) are most useful.
+ * @param atBegin - Optional, defaults to false. If atBegin is false or
+ * unspecified, callback will only be executed `delay` milliseconds after
+ * the last debounced-function call. If atBegin is true, callback will
+ * be executed only at the first debounced-function call.
+ * (After the throttled-function has not been called for `delay` milliseconds,
+ * the internal counter is reset).
+ * @param  callback - A function to be executed after delay milliseconds.
+ * The `this` context and all arguments are passed through, as-is,
+ * to `callback` when the debounced-function is executed.
+ * @returns A new, debounced function.
+ * @see {@link throttle}
  */
 export function debounce(
   callback: Fn<any>,
   delay: number,
-  atBegin: boolean,
+  atBegin = false,
 ): any | void {
   return callback === undefined
     ? throttle(callback, delay, atBegin, false)
