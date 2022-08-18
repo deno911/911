dc4830489a47c7f5deaef298200b2477bf599307
Nicholas Berlette <nick@berlette.com>
2022-08-18 14:17:06 -0700

    chore: convert mod.ts to explicit named exports

--- mod.ts
+++ mod.ts
@@ -1,20 +1,197 @@
 /// <reference no-default-lib="true">
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.window" />
+/// <reference lib="dom" />
+/// <reference lib="esnext" />
+
+import {
+  dayOfYear,
+  difference,
+  format,
+  isLeap,
+  parse,
+  relative,
+  toIMF,
+  weekOfYear,
+} from "./src/date.ts";
+
+export const date = {
+  parse,
+  format,
+  difference,
+  relative,
+  toIMF,
+  isLeap,
+  dayOfYear,
+  weekOfYear,
+};
+
+export { Times, TimesAbbr } from "./src/date.ts";
+
+export { CACHE_MAX_SIZE, createCache, globalCache } from "./src/cache.ts";
+
+export {
+  aggregateGroups,
+  assign,
+  associateBy,
+  associateWith,
+  at,
+  chunk,
+  clampArrayFrom,
+  clampArrayRange,
+  clampArrayTo,
+  clearUndefined,
+  deeperMerge,
+  type DeepMerge,
+  deepMerge,
+  diff,
+  distinct,
+  distinctBy,
+  ensureArray,
+  filterEntries,
+  filterInPlace,
+  filterKeys,
+  filterValues,
+  flatten,
+  flattenArrayable,
+  groupBy,
+  hasOwnProperty,
+  intersect,
+  isKeyOf,
+  last,
+  mapEntries,
+  mapKeys,
+  mapNotNullish,
+  mapValues,
+  maxBy,
+  maxOf,
+  maxWith,
+  merge,
+  mergeArrayable,
+  minBy,
+  minOf,
+  minWith,
+  move,
+  objectEntries,
+  objectKeys,
+  objectMap,
+  objectPick,
+  partition,
+  partitionBy,
+  pick,
+  range,
+  remove,
+  removeEmptyValues,
+  sample,
+  shuffle,
+  sortBy,
+  type SortCompareFn,
+  sortFactory,
+  sortInPlace,
+  type SortKeyFn,
+  toArray,
+  union,
+  uniq,
+  unzip,
+  zip,
+} from "./src/collection.ts";
+
+export {
+  ansi,
+  camelCase,
+  cases,
+  constantCase,
+  decode,
+  encode,
+  kebabCase,
+  lowerCase,
+  paramCase,
+  pascalCase,
+  prettyBytes,
+  printf,
+  sentenceCase,
+  slugify,
+  snakeCase,
+  sprintf,
+  Templette,
+  titleCase,
+  upperCase,
+} from "./src/fmt.ts";
+
+export {
+  eTag,
+  etag,
+  sha1,
+  sha256,
+  sha512,
+  toHex,
+  utf8TextDecoder,
+  utf8TextEncoder,
+  uuid,
+} from "./src/hash.ts";
+
+export {
+  type ConnInfo,
+  getContentType,
+  handleRequest,
+  jsx,
+  lookupMediaType,
+  type ResponseProps,
+  type Routes,
+  serve,
+  type ServeInit,
+  serveStatic,
+  type ServeStaticOptions,
+  Status,
+  STATUS_TEXT,
+  toResponse,
+  validateRequest,
+} from "./src/http.ts";
+
+export { json } from "./src/json.ts";
+
+export { log, Logger, LogLevel, type LogLevelName } from "./src/log.ts";
+
+export { clamp, randomInteger, sum } from "./src/math.ts";
+
+export {
+  type MemoizableFn,
+  memoize,
+  memoized,
+  type MemoizeOptions,
+} from "./src/memoize.ts";
+
+export { Params, type ParamsInit, type ParamsOptions } from "./src/params.ts";
+
+export {
+  type ControlledPromise,
+  createControlledPromise,
+  createPromiseLock,
+  createSingletonPromise,
+  p,
+  Promises,
+} from "./src/promises.ts";
+
+export { SerializeMap } from "./src/serialize_map.ts";
+
+export { SerializeSet } from "./src/serialize_set.ts";
+
+export { State } from "./src/state.ts";
+
+export {
+  is,
+  isArray,
+  isArrayLike,
+  isFalsey,
+  isNotNull,
+  isObject,
+  isTruthy,
+  notNull,
+  notNullish,
+  notUndefined,
+  toStringTag,
+} from "./src/type.ts";
+
+export { doc, dotenv } from "./deps.ts";
 
 export * as default from "./mod.ts";
-export * from "./src/cache.ts";
-export * from "./src/collection.ts";
-export * as date from "./src/date.ts";
-export * from "./src/fmt.ts";
-export * from "./src/hash.ts";
-export * from "./src/http.ts";
-export * from "./src/json.ts";
-export * from "./src/log.ts";
-export * from "./src/math.ts";
-export * from "./src/memoize.ts";
-export * from "./src/params.ts";
-export * from "./src/promises.ts";
-export * from "./src/serialize_map.ts";
-export * from "./src/serialize_set.ts";
-export * from "./src/state.ts";
-export * from "./src/type.ts";
-export { doc, dotenv } from "./deps.ts";
